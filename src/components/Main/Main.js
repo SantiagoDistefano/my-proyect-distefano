6cf8a4701d69ee932a12c41aed0fba692f52eaaf
“santi <santidistefano12@gmail.com>
2021-05-30 15:09:55 -0300

    actualizacion2

---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import './Main.css';
+import NavBar from "../NavBar/NavBar"
+
+function Main() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+export default Main;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import "./NavBar.css"
+
+function NavBar() {
+    return (
+        <nav class="highNav">
+                <a href="#section1">Seccion 1</a>
+                <a href="#section2">Seccion 2</a>
+                <h3>Peluchin</h3>  
+        </nav>
+
+    );
+}
+
+
+
+export default NavBar;
