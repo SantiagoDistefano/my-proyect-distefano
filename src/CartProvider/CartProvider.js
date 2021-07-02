2bb07da1443767151fa0b81ed2acc8212901c15b
“santi <santidistefano12@gmail.com>
2021-07-02 10:30:56 -0300

    actualizacion10

---
+++ src/CartProvider/CartProvider.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import CartContext from "../Context/CartContext";
+
+const CartProvider = ({ defaultValue = [], children }) => {
+  const [cart, setCart] = useState(defaultValue);
+
+  const addItem = (item, quantity) => {
+    if (!isInCart(item.id)) {
+      setCart([...cart, { item, quantity }]);
+    } else {
+      let product = cart.find((y) => y.item.id === item.id);
+      product.quantity += quantity;
+
+      setCart(
+        cart.map((item) =>
+          item.item.id === item.id
+            ? { ...item.item, quantity: product.quantity }
+            : item
+        )
+      );
+    }
+  };
+
+  const removeItem = (id) => {
+    if (isInCart(id)){
+
+    }
+    
+  };
+
+  const clear = () => {
+    setCart([]);
+  };
+
+  const isInCart = (id) => {
+    return getFromCart(id);
+  };
+
+  const getFromCart = (id) => {
+    return cart.find((y) => y.item.id === id);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{ cart, addItem, removeItem, clear, isInCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,32 +3,33 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
 import ItemListContainer from "../ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "../ItemDetailContainer/ItemDetailContainer";
-import CartContext from "../../Context/CartContext";
-
-
-
+import CartProvider from "../../CartProvider/CartProvider";
+import Cart from "../Cart/Cart";
 
 function Main() {
   return (
-    <CartContext.Provider value={[]}>
-    <div className="App">
-      <BrowserRouter>
-        <NavBar />
-        <Switch>
-          <Route exact path="/">
-            <ItemListContainer />
-          </Route>
-          <Route exact path='/category/:category'>
-            <ItemListContainer />
-          </Route>
-          <Route exact path="/item/:id">
-            <ItemDetailContainer />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-    </CartContext.Provider>
+    <CartProvider value={[]}>
+      <div className="App">
+        <BrowserRouter>
+          <NavBar />
+          <Switch>
+            <Route exact path="/">
+              <ItemListContainer />
+            </Route>
+            <Route exact path="/category/:category">
+              <ItemListContainer />
+            </Route>
+            <Route exact path="/item/:id">
+              <ItemDetailContainer />
+            </Route>
+            <Route exact path="/cart">
+              <Cart />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </CartProvider>
   );
-};
+}
 
 export default Main;
